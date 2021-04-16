b583b58f75bb679cd6f999c1e9c2ed8fbfcd0be8
chaythang <82649391+chaythang@users.noreply.github.com>
2021-04-16 19:15:46 +0700

    Update install_centos8.sh

--- install_centos8.sh
+++ install_centos8.sh
@@ -116,7 +116,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 echo "How many proxy do you want to create? Example 500"
 read COUNT
 
-FIRST_PORT=10000
+FIRST_PORT=3000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
