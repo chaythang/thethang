8637811c4a751a1471d9b95703a9a0b49d950361
chaythang <82649391+chaythang@users.noreply.github.com>
2021-04-19 13:55:37 +0700

    Update install_centos8.sh

--- install_centos8.sh
+++ install_centos8.sh
@@ -113,7 +113,7 @@ IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
+echo "How many proxy do you want to create? Example ert"
 read COUNT
 
 FIRST_PORT=3000
